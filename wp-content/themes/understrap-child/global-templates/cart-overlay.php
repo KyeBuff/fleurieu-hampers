f154ea6511c511f50ddfb927ba1e43f4b4ca3124
Kye Buffery <kyebuff@Kyes-MacBook-Pro.local>
2018-08-05 21:21:10 +0100

    fixed bugs

--- wp-content/themes/understrap-child/global-templates/cart-overlay.php
+++ wp-content/themes/understrap-child/global-templates/cart-overlay.php
@@ -6,15 +6,12 @@
  */
 ?>
 
-<div class="cart-preview" id="cart-preview">
+<div class="cart-preview d-none" id="cart-preview">
 	<div class="cart-preview__content">
 		<div class="cart-preview__heading">
 			<button id="close-cart" class="cart-preview__close" id="close-cart-preview"><i class="fa fa-chevron-right"></i></button>
 			<p class="cart-preview__header text-center">Cart</p>
 		</div>
 		<?php woocommerce_mini_cart(); ?>
-		<!-- <div class="cart-preview__footer">
-			<a href="/checkout" class="cart-preview__checkout">Checkout</a>
-		</div> -->
 	</div>
 </div>
