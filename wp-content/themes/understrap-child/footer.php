50982d25fcff3053eb4123117768e26df131a177
Kye Buffery <kyebuff@Kyes-MacBook-Pro.local>
2018-08-05 21:18:43 +0100

    finished side cart

--- wp-content/themes/understrap-child/footer.php
+++ wp-content/themes/understrap-child/footer.php
@@ -63,8 +63,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 </div><!-- #page we need this extra closing tag here -->
 
 <?php wp_footer(); ?>
-
 </body>
 
 </html>
 
+		<?php woocommerce_mini_cart(); ?>
--- wp-content/themes/understrap-child/global-templates/cart-overlay.php
+++ wp-content/themes/understrap-child/global-templates/cart-overlay.php
@@ -4,17 +4,17 @@
  *
  * @package understrap
  */
-
 ?>
 
-<div class="cart-preview d-none" id="cart-preview">
+<div class="cart-preview" id="cart-preview">
 	<div class="cart-preview__content">
 		<div class="cart-preview__heading">
 			<button id="close-cart" class="cart-preview__close" id="close-cart-preview"><i class="fa fa-chevron-right"></i></button>
 			<p class="cart-preview__header text-center">Cart</p>
 		</div>
-		<div class="cart-preview__footer">
+		<?php woocommerce_mini_cart(); ?>
+		<!-- <div class="cart-preview__footer">
 			<a href="/checkout" class="cart-preview__checkout">Checkout</a>
-		</div>
+		</div> -->
 	</div>
-</div><
+</div>
