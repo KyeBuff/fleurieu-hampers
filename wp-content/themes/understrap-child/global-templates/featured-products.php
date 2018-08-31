8197f15861f23f8b8d1fba8ec96a51cbd4afe3e5
Kye Buffery <kyebuffery14@gmail.com>
2018-08-31 19:56:05 +0100

    fixed slick undefined

--- wp-content/themes/understrap-child/global-templates/featured-products.php
+++ wp-content/themes/understrap-child/global-templates/featured-products.php
@@ -9,9 +9,11 @@ $featured_products = get_field('products');
 	<div class="featured-products__carousel">
 		<?php foreach ($featured_products as $product) {
 			$product = $product['product'];
-		    $product_meta = get_post_meta($product->ID);?>
+		    $product_meta = get_post_meta($product->ID);
+		    $image_id = isset($product_meta['_thumbnail_id']) ? $product_meta['_thumbnail_id'] : null;
+		    ?>
 			<a class="slick-link" href="<?php echo get_the_permalink($product->ID); ?>">
-			<?php echo wp_get_attachment_image($product_meta['_thumbnail_id'][0], 'all'); ?>
+			<?php echo $image_id ? wp_get_attachment_image($image_id[0], 'all') : null; ?>
 			</a>
 		<?php } ?>
 	</div>
--- wp-content/themes/understrap-child/global-templates/popular-products.php
+++ wp-content/themes/understrap-child/global-templates/popular-products.php
@@ -14,9 +14,11 @@ $products = $query->get_products();
 	<h2 class="section__header">Most Popular</h2>
 	<div class="featured-products__carousel">
 		<?php foreach ($products as $product) {
-		    $product_meta = get_post_meta($product);?>
+		    $product_meta = get_post_meta($product);
+		    $image_id = isset($product_meta['_thumbnail_id']) ? $product_meta['_thumbnail_id'] : null;
+		    ?>
 			<a class="slick-link" href="<?php echo get_the_permalink($product); ?>">
-			<?php echo wp_get_attachment_image($product_meta['_thumbnail_id'][0], 'all'); ?>
+			<?php echo $image_id ? wp_get_attachment_image($image_id[0], 'all') : null; ?>
 			</a>
 		<?php } ?>
 	</div>
